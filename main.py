2b836443670fd9a4b4d284946037af6eb2015931
Deepanshu Tiwari <tiwarideepanshu235@gmail.com>
2021-06-12 14:09:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -234,12 +234,6 @@ while True:
         multiplayer = int(input('do you want to play against human player or computer:(for human player enter 1 for computer player enter 2) '))
 
     if multiplayer==1:
-        play_game = input('Are you ready to play? Enter Yes or No.')
-    
-        if play_game.lower() == 'yes':
-            game_on = True
-        else:
-            game_on = False
         player1_marker, player2_marker = player_input()
         turn = choose_first()
         print(turn + ' will go first.')
